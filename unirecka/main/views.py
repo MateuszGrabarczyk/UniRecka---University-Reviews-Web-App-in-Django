262aa22a5ed0fff04d0f9006958f2d254e679a98
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-07-01 07:03:59 +0200

    ✨Add about page

--- unirecka/main/views.py
+++ unirecka/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "main/index.html")
+
+def about(request):
+    return render(request, "main/about.html")
