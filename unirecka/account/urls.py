ed143e5fad480db54ebaf3fd2c1a032cc9cf4789
Mateusz Grabarczyk <stepowa28@gmail.com>
2023-07-01 15:32:19 +0200

    ✨Add registration page, allow it as well

--- unirecka/account/urls.py
+++ unirecka/account/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- unirecka/account/views.py
+++ unirecka/account/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():           
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return redirect('index')
+            # return render(request, 'account/register_done.html', {
+            #     'new_user': new_user
+            # })
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {
+        'user_form': user_form
+    })
+
 
 def user_login(request):
     if request.method == 'POST':
