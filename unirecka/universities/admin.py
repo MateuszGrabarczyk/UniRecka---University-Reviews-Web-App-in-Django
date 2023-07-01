0c1fa87871238549ffeb00dd81a6c607035c56dd
Mateusz Grabarczyk <stepowa28@gmail.com>
2023-07-01 18:36:53 +0200

    ✨Add Review model and register it to admin

--- unirecka/universities/admin.py
+++ unirecka/universities/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import University
+from .models import University, Review
 
 # Register your models here.
 admin.site.register(University)
+admin.site.register(Review)
