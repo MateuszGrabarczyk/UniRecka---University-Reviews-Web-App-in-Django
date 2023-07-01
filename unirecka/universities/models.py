20c0ba0314f8c2a569066b7751c33a3c1a5f4a5f
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-07-01 18:36:53 +0200

    ✨Add Review model and register it to admin

--- unirecka/universities/models.py
+++ unirecka/universities/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 class University(models.Model):
     name = models.CharField(max_length=150)
     voivodeship = models.CharField(max_length=150)
@@ -9,3 +10,15 @@ class University(models.Model):
         verbose_name = 'University'
         verbose_name_plural = 'Universities'
 
+class Review(models.Model):
+    title = models.CharField( max_length=150)
+    description = models.CharField( max_length=150)
+    rating = models.IntegerField(validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ])
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
