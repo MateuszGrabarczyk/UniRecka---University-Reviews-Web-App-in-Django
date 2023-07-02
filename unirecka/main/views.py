539af3047c51fb73329704bb68cbe8c82e0e434e
Mateusz Grabarczyk <stepowa28@gmail.com>
2023-07-02 19:16:52 +0200

    ✨Allow clicking best Polish universities and redirect to their pages

--- unirecka/main/views.py
+++ unirecka/main/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from universities.models import University
 # Create your views here.
 def index(request):
-    return render(request, "main/index.html")
+    jg = University.objects.filter(name="Uniwersytet Jagielloński w Krakowie").first()
+    uw = University.objects.filter(name="Uniwersytet Warszawski").first()
+    agh = University.objects.filter(name="Akademia Górniczo-Hutnicza im. Stanisława Staszica w Krakowie").first()
+    return render(request, "main/index.html", {
+        "jg": jg,
+        "uw": uw,
+        "agh": agh
+    })
 
 def about(request):
     return render(request, "main/about.html")
