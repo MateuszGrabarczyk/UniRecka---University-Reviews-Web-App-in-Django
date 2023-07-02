c872bb507723a8f7901de161210b186802bbca0d
Mateusz Grabarczyk <stepowa28@gmail.com>
2023-07-02 18:34:47 +0200

    ✨Add profile page with editing username and email

--- unirecka/account/urls.py
+++ unirecka/account/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register, name='register'),
+    path('profile/<int:user_id>/', views.profile, name='profile'),
 ]
