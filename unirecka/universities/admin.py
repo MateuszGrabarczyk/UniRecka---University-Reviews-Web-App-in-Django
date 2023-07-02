1098a45180ca9ceb051e4e4909c58ff5637d8698
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-07-02 08:53:43 +0200

    💄Style admin, add filtering

--- unirecka/universities/admin.py
+++ unirecka/universities/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from .models import University, Review
 
-# Register your models here.
-admin.site.register(University)
-admin.site.register(Review)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'voivodeship')
+    list_filter = ('name', 'city', 'voivodeship')
+
+    def __str__(self):
+        return self.name
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'rating', 'university', 'user')
+    list_filter = ('rating', 'university', 'user')  
+
+admin.site.register(University, UniversityAdmin)
+admin.site.register(Review, ReviewAdmin)
