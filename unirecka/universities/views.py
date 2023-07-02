34a79ae5b512c66ca18e7112a7d2b225c4f65e04
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-07-02 07:49:56 +0200

    ✨Add navigation to university detail page

--- unirecka/universities/views.py
+++ unirecka/universities/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from .models import University
 
 def university_list(request):
@@ -6,3 +7,8 @@ def university_list(request):
     return render(request, 'universities/university_list.html', {
         'universities': universities
     })
+
+class UniversityDetailView(DetailView):
+    model = University
+    template_name = 'universities/university_detail.html'  # Replace with your desired template name
+    context_object_name = 'university'
