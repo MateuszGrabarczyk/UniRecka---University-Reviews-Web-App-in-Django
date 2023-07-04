14002c7efcf498b9ff3babe7c06f6e04c06ffaf0
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-07-04 06:59:21 +0200

    🥅Show error message when invalid login or register

--- unirecka/universities/urls.py
+++ unirecka/universities/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.university_list, name='university_list'),
     path('<int:pk>/', views.UniversityDetailView.as_view(), name='university_detail'),
-    path('review/create/<int:university_id>/', views.ReviewCreateView.as_view(), name='review_create'),
-    path('review/report/<int:review_id>/', views.ReviewReportCreateView.as_view(), name='review_report_create'),
+    path('review/create/<int:university_id>/', login_required(views.ReviewCreateView.as_view()), name='review_create'),
+    path('review/report/<int:review_id>/', login_required(views.ReviewReportCreateView.as_view()), name='review_report_create'),
 ]
