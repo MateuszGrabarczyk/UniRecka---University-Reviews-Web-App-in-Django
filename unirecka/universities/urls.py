36dfed76ae1fc6b39a21429a5df9a5168c42c2e8
Mateusz Grabarczyk <stepowa28@gmail.com>
2023-07-04 15:43:14 +0200

    ✨Add deleting your review in profile page

--- unirecka/universities/urls.py
+++ unirecka/universities/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.university_list, name='university_list'),
     path('<int:pk>/', views.UniversityDetailView.as_view(), name='university_detail'),
     path('review/create/<int:university_id>/', login_required(views.ReviewCreateView.as_view()), name='review_create'),
+    path('review/delete/<int:pk>/', views.ReviewDeleteView.as_view(), name='review_delete'),
     path('review/report/<int:review_id>/', login_required(views.ReviewReportCreateView.as_view()), name='review_report_create'),
     path('review/like', views.review_like, name='like')
 ]
