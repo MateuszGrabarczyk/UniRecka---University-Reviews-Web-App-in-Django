fea131ef1797a0f58c3438ae9db4ab86a0208ebb
Mateusz Grabarczyk <stepowa28@gmail.com>
2023-07-07 09:46:48 +0200

    🔐Hide crucial buttons when user is not logged in

--- unirecka/universities/urls.py
+++ unirecka/universities/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path('review/create/<int:university_id>/', login_required(views.ReviewCreateView.as_view()), name='review_create'),
     path('review/delete/<int:pk>/', login_required(views.ReviewDeleteView.as_view()), name='review_delete'),
     path('review/edit/<int:pk>/', login_required(views.ReviewUpdateView.as_view()), name='review_update'),
-    path('review/report/<int:review_id>/', login_required(views.ReviewReportCreateView.as_view()), name='review_report_create'),
+    path('review/report/<int:review_id>/', views.ReviewReportCreateView.as_view(), name='review_report_create'),
     path('review/like', views.review_like, name='like'),
     path('review/create/comment/<int:review_id>/', login_required(views.CommentCreateView.as_view()), name='comment_create'),
     path('review/edit/comment/<int:pk>/', login_required(views.CommentUpdateView.as_view()), name='comment_update'),
     path('review/delete/comment/<int:pk>/', login_required(views.CommentDeleteView.as_view()), name='comment_delete'),
-    path('review/report/<int:review_id>/comment/<int:comment_id>/', login_required(views.CommentReportCreateView.as_view()), name='comment_report_create'),
+    path('review/report/<int:review_id>/comment/<int:comment_id>/', views.CommentReportCreateView.as_view(), name='comment_report_create'),
 ]
