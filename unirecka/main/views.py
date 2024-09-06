27a4a93d6913381c6a66bf28ed3b8915f0a9e9cf
Mateusz Grabarczyk <stepowa28@gmail.com>
2024-09-06 08:46:43 +0200

    ♻️Refactor main app

--- unirecka/main/views.py
+++ unirecka/main/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-
 from universities.models import University
 
 
 def index(request):
-    jg = University.objects.filter(name="Uniwersytet Jagielloński w Krakowie").first()
-    uw = University.objects.filter(name="Uniwersytet Warszawski").first()
-    agh = University.objects.filter(
-        name="Akademia Górniczo-Hutnicza im. Stanisława Staszica w Krakowie"
-    ).first()
-    return render(request, "main/index.html", {"jg": jg, "uw": uw, "agh": agh})
+    universities = {
+        "jg": University.objects.filter(
+            name="Uniwersytet Jagielloński w Krakowie"
+        ).first(),
+        "uw": University.objects.filter(name="Uniwersytet Warszawski").first(),
+        "agh": University.objects.filter(
+            name="Akademia Górniczo-Hutnicza im. Stanisława Staszica w Krakowie"
+        ).first(),
+    }
+    return render(request, "main/index.html", universities)
 
 
 def about(request):
